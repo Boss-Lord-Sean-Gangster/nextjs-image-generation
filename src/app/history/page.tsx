3b36b3004fef069116b5aa5345e8802ae95ffd5a
Boss-Lord-Sean-Gangster <153370049+Boss-Lord-Sean-Gangster@users.noreply.github.com>
2024-07-21 14:34:28 +0530

    Update page.tsx

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Navbar from '../Navbar';
 
-const HistoryPage = () => {
+const Page = () => {
   return (
     <div>
       <Navbar />
-      <h2 className='text-center text-lime-500'>Coming Soon....</h2>
+      <div className='flex justify-center items-center min-h-screen'>
+        <h2 className='text-center text-lime-500 text-2xl md:text-3xl'>Coming Soon....</h2>
+      </div>
     </div>
   );
-}
+};
 
-export default HistoryPage;
+export default Page;
