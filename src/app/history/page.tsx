04262e8904804b5d0fad523ddcd20af49645ddb0
Boss-Lord-Sean-Gangster <153370049+Boss-Lord-Sean-Gangster@users.noreply.github.com>
2024-07-21 14:04:42 +0530

    Update page.tsx

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -4,10 +4,10 @@ import Navbar from '../Navbar'
 const Page = () => {
   return (
     <div>
-        <Navbar/>
-        <h2 className='text-center text-lime-500'>Coming Soon....</h2>
-        </div>
+      <Navbar/>
+      <h2 className='text-center text-lime-500'>Coming Soon....</h2>
+    </div>
   )
 }
 
-export default Page
+export default Page;
